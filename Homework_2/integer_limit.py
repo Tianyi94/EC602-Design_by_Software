b7e4f5ce1cedd35a1ebf605881fb5e2695af29f9
GrazianoMJ <GrazianoMJ@users.noreply.github.com>
2017-09-12 12:12:05 -0400

    interger_limit.py Completion:
    * Added for loop to print out the bytes, maximum unsigned, min and max signed integers for 1 to 8.
    * Addressed issues with code that caused errors:
    ..- Parenthesis missing from print() statements.

--- Homework_2/integer_limit.py
+++ Homework_2/integer_limit.py
@@ -1,10 +1,15 @@
 # Copyright 2017 Michael Graziano mjgrazia@bu.edu
 
 Table = "{:<6}{:<22}{:<22}{:<22}" # Format string for output table
-
 print(Table.format("Bytes",
                    "Largest Unsigned Int",
                    "Minimum Signed Int",
-                   "Minimum Signed Int",
                    "Maximum Signed Int"
-                  )
+                  ))
+
+for byte in range(1, 9):
+    n = byte * 8
+    max_uint = 2**n - 1
+    min_sint = -2**(n-1)
+    max_sint = 2**(n-1) - 1
+    print(Table.format(byte, max_uint, min_sint, max_sint))
