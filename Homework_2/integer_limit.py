50f7c9baedcfad82a7f4762ad84bdf4fc4535346
GrazianoMJ <GrazianoMJ@users.noreply.github.com>
2017-09-12 10:51:03 -0400

    Homework 2 File Addition, Examples and Assignment Files
    * Created the Homework 2 Folder to hold all items for the second assignment.
    * Created Examples Folder to hold all examples used during the Assignment description.
    * Imported rounding_errors.cpp & timed_example.cpp into the Examples folder and compiled.
    * Created the empty files earth_electrons.cpp, earth_electrons.py, integer_limit.py, number_limits.cpp and overflow_times.cpp
    * Created the initial Table variable and output header for integer_limit.py

---
+++ Homework_2/integer_limit.py
@@ -0,0 +1,10 @@
+# Copyright 2017 Michael Graziano mjgrazia@bu.edu
+
+Table = "{:<6}{:<22}{:<22}{:<22}" # Format string for output table
+
+print(Table.format("Bytes",
+                   "Largest Unsigned Int",
+                   "Minimum Signed Int",
+                   "Minimum Signed Int",
+                   "Maximum Signed Int"
+                  )
