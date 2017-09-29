7cca51503cd490674d4b9ae2c0f8fd0f3bb58721
GrazianoMJ <GrazianoMJ@users.noreply.github.com>
2017-09-29 18:08:57 -0400

    Structure Change / Add & Subtraction Update
    * Changed from two linked lists to a single dictionary to hold polynomial representation
    * Changed initializer, add, and str modules to address the change to dictionary
    * Created subtraction method that is able to utilize the add function after changing all values in the passed Polynomial to negative.

--- Homework_4/modeling_polynomial.py
+++ Homework_4/modeling_polynomial.py
@@ -6,14 +6,57 @@ class Polynomial():
         """ __init__: Creates the container for the polynomial based on the 
             provided  list coefs
         """
-        self.coefs, self.expon = ([], [])
+        
+        self.express = {} # Dictionary for storing
+
         if not coefs:
-            self.coefs = [0]
-            self.expon = [0]
+            self.express.update({0:0})
         else:
-            for value in reversed(coefs): 
-                self.coefs.append(value)
-                self.expon.append(list(reversed(coefs)).index(value))
+            for i in range(len(coefs) - 1, -1, -1): 
+                self.express.update({i : list(reversed(coefs))[i]})
+
+    def __add__(self, poly):
+
+        """ __add__: Updates the '+' symbol in order to allow for the 
+            expression Poly 1 + Poly 2 to produce a valid result.
+        """
+
+        new_express = self.express.copy()
+        new_express.update(poly.express)
+
+        for expon in new_express.keys():
+            if expon in self.express and expon in poly.express:
+                new_express[expon] = self.express[expon] + poly.express[expon]
+            elif expon in self.express and expon not in poly.express:
+                new_express[expon] = self.express[expon]
+            else:
+                pass
+
+        for expon in list(reversed(new_express.keys())):
+            if new_express[expon] == 0:
+                new_express.pop(expon)
+            else:
+                break
+
+        return Polynomial(list(reversed(new_express.values())))
+
+    def __sub__(self, poly):
+
+        """ __sub__: Updates the '-' symbol in order to allow for the
+            express Poly 1 + Poly 2 to produce a valid result.
+        """
+    
+        return self.__add__(Polynomial([-x for x in list(reversed(
+                            poly.express.values()
+                           ))]))
+        
+    def __len__(self):
+        
+        """ __len__: Provides the length of the of the polynomial when the 
+            built in function len() is called.
+        """
+        
+        return len(self.express)
 
     def __str__(self):
 
@@ -21,10 +64,7 @@ class Polynomial():
             when called from str(Polynomial), print() and format().
         """
 
-        return "".join("{}*x^{} + ".format(self.coefs[i], self.expon[i]) \
-               if i > 0 \
-               else "{}*x^{}".format(self.coefs[i], self.expon[i]) \
-               for i in range(len(self.coefs)-1,-1,-1))
+        return str(self.express)
 
 def main():
     # Polynomial Creation Practice
@@ -33,11 +73,17 @@ def main():
             Polynomial([5, 6, 8]),
             Polynomial([7, 9, 2, 4])
            ]
+    adder = Polynomial([1, 2, 3])
+    diff = Polynomial([5, 3, 8])
     output = []
 
     # Polynomial "Pretty Print" Practice
     for item in test:
         print(item)
+        print(adder)
+        print(item + adder)
+        print(diff)
+        print(item - diff)
 
 if __name__=="__main__":
 	main()
